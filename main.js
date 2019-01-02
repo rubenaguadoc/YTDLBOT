041fbdf0341a37f2fd5e3826c77981bd0b5ad260
Rubén <rubenaguadoc@gmail.com>
2019-01-03 00:56:35 +0100

    More fixes

--- main.js
+++ main.js
@@ -172,7 +172,10 @@ async function getVideos(service, auth, uploads) {
 async function downloadVideo(url, resolution) {
   return new Promise((resolve, reject) => {
     exec(
-      `pipenv run python ${path.join(__dirname, 'main.py')} ${resolution} ${url}`,
+      `$(cd ${__dirname} && pipenv --venv)/bin/python ${path.join(
+        __dirname,
+        'main.py',
+      )} ${resolution} ${url}`,
       (err, stdout, stderr) => {
         if (err) reject(`${err}\n\nstdout: ${stdout}\n\nstderr: ${stderr}`);
         else resolve();
