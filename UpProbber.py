af82d76dad3a4af8d55e1dd4800a594fb10f8af0
Rubén <rubenaguadoc@gmail.com>
2019-01-03 00:08:08 +0100

    Fixed UpProbber

--- UpProbber.py
+++ UpProbber.py
@@ -1,9 +1,11 @@
 import subprocess
 
-ps = subprocess.Popen(('ps', '-u', 'root'), stdout=subprocess.PIPE)
-output = subprocess.check_output(('grep', 'node'), stdin=ps.stdout)
-ps.wait()
+try:
+    ps = subprocess.Popen(('ps', '-u', 'root'), stdout=subprocess.PIPE)
+    output = subprocess.check_output(('grep', 'node'), stdin=ps.stdout)
+    ps.wait()
 
-if output.decode('utf-8').count('node') < 1:
-    result = subprocess.Popen(
-        ['/usr/bin/node', '/home/pi/srv/YTDLBOT/main.js'])
+    if output.decode('utf-8').count('node') < 1:
+        raise Exception()
+except:
+    subprocess.Popen(['/usr/bin/node', '/home/pi/srv/YTDLBOT/main.js'])
